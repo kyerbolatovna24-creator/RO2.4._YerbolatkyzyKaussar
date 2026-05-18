05d7f81b42fb5676749143a9f49bef4b858ed7b6
ThinkStation31 <ThinkStation31@DESKTOP-1H68PV3>
2026-05-18 18:54:03 +0500

    task 2 completed

---
+++ SchoolApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnOpenStudentsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(StudentsPage));
+    }
+}
