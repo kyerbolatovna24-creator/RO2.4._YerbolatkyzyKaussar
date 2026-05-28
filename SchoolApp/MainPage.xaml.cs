fdc36734a5494503516187844c968516fd640c00
ThinkStation31 <ThinkStation31@DESKTOP-1H68PV3>
2026-05-28 17:05:54 +0500

    task3 completed

--- SchoolApp/MainPage.xaml.cs
+++ SchoolApp/MainPage.xaml.cs
@@ -7,12 +7,8 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-
         BindingContext = new Person();
     }
 
-    private async void OnOpenStudentsClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new StudentsPage());
-    }
+    // existing OnOpenStudentsClicked from L14 stays here unchanged
 }
